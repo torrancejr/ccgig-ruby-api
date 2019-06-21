cb4d0dee17f1697f0e0a9181d27f7fb84755cbe3
Ryan Torrance <torrance.jr@gmail.com>
2019-06-21 09:57:45 -0700

    first commt

---
+++ db/migrate/20190621165404_devise_create_users.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string "first_name", null: false
+      t.string "last_name", null: false
+      t.string "username", null: false
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer "sign_in_count", default: 0, null: false
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.inet "current_sign_in_ip"
+      t.inet "last_sign_in_ip"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "phone"
+      t.integer "rating"
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
